726e926e201847db9156adcac1f620a247cc85e6
LunaEnzo875 <enzoluna760@gmail.com>
2025-05-30 11:37:36 -0300

    eventos.cs

--- eventos.cs
+++ eventos.cs
@@ -4,18 +4,17 @@ public class Eventos
 {
     private string nombre="";
     private string descripcion="";
-    private int tipoEvento;
+    private string tipoEvento;
     private DateTime inicio;
     private DateTime fin;
     private int capacidadMaxima = 100;
     private int precioEntrada = 35;
     private string ubicacion="";
 
-    public Eventos(string? nombre, string? descripcion, int tipoEvento, DateTime inicio, DateTime fin, string? ubicacion)
+    public Eventos(string? nombre, string? descripcion, string tipoEvento, DateTime inicio, DateTime fin, string? ubicacion)
     {
 this.nombre = nombre;
 this.descripcion = descripcion;
-Validaciones.ValidarEvento(tipoEvento);
 this.tipoEvento = tipoEvento;
 this.inicio = inicio;
 this.fin = fin;
@@ -23,20 +22,7 @@ this.capacidadMaxima = capacidadMaxima;
 this.precioEntrada = precioEntrada;
 this.ubicacion = ubicacion;
     }
-    public void TipoDeEventos()
-    {
-        if (tipoEvento == 1)
-        Console.WriteLine("Cata");
 
-        else if(tipoEvento == 2)
-        Console.WriteLine("feria");
-
-        else if(tipoEvento == 3)
-        Console.WriteLine("Clase");
-
-        else if(tipoEvento == 4)
-        Console.WriteLine("Cena tematica");
-    }
 
     public string getNombre()
     {
@@ -45,7 +31,7 @@ this.ubicacion = ubicacion;
     public string getdescripcion(){
         return this.descripcion;
     }
-    public int gettipoEvento(){
+    public string gettipoEvento(){
         return this.tipoEvento;
     }
     public DateTime getinicio(){
