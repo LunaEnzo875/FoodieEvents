19b1b8786ee2be0bedf3aecd008dba573daaede9
LunaEnzo875 <enzoluna760@gmail.com>
2025-05-30 11:36:24 -0300

    ChefOrganizadores.cs

--- ChefOrganizadores.cs
+++ ChefOrganizadores.cs
@@ -1,38 +1,30 @@
-namespace FoodieEvents;
 
-public class ChefOrganizadores{
+//Participantes
+List<Participantes> participantesList = new List<Participantes>
+{
 
-    private string NombreCompleto="";
-    private int especialidad;
-    private int nacionalidad;
-    private int cantidadExperiencia;
-    private string Correo="";
-    private int Telefono;
-public ChefOrganizadores(string nombreCompleto,int especialidad,int nacionalidad,int cantidadExperiencia,string Correo,int Telefono){
-this.NombreCompleto=nombreCompleto;
-this.especialidad=especialidad;
-this.nacionalidad=nacionalidad;
-this.Correo=Correo;
-this.Telefono=Telefono;
-}
-    public string getNombreCompleto()
-    {
-        return this.NombreCompleto;
-    }
-    public int getespecialidad(){
-        return this.especialidad;
-    }
-    public int getNacionalidad(){
-        return this.nacionalidad;
-    }
-    public int getcantidadExperiencia(){
-        return this.cantidadExperiencia;
-    }
-    public string GetCorreo()
-    {
-        return this.Correo;
-    }
-    public int GetTelefono(){
-        return this.Telefono;
-    }
-}
+};
+
+Console.WriteLine("Ingrese su nombre completo de participante");
+string  nombreCompleto=Console.ReadLine();
+
+Console.WriteLine("Ingrese su correo del participante");
+string correo=Console.ReadLine();
+Validaciones.Validarcorreo(correo);
+
+Console.WriteLine("Ingrese su numero de telefono de participante");
+int telefono=int.Parse(Console.ReadLine());
+Validaciones.ValidarTelefono(telefono);
+
+Console.WriteLine("Ingrese su DNI");
+int DNI=int.Parse(Console.ReadLine());
+
+Console.WriteLine("¿Tiene una restriccion alimentaria?");
+string restriccionAlimentaria=Console.ReadLine();
+
+Participantes participantes=new Participantes(nombreCompleto,correo,telefono,DNI,restriccionAlimentaria);
+Console.WriteLine("El nombre Completo:"+participantes.getnombreCompleto());
+Console.WriteLine("El correo electronico es:"+participantes.getcorreo());
+Console.WriteLine("Su numero de telefono es:"+participantes.gettelefono());
+Console.WriteLine("Su DNI es:"+ participantes.getDNI());
+Console.WriteLine("Tiene restriccion Alimentaria?:"+ participantes.getrestriccionAlimentaria());
